6dbca4cc1a98104912a7a44d34f637d1722d04d3
Stephen Valdinger <stephen@chocolatey.io>
2025-06-10 08:23:01 -0400

    fixing action stuff

--- issueparser.ps1
+++ issueparser.ps1
@@ -17,12 +17,12 @@ process {
     foreach ($match in $matches) {
         $key = $match.Groups[1].Value -replace "\s+", ""  # Remove spaces for the hashtable key
         $value = $match.Groups[2].Value.Trim()           # Trim extra spaces/newlines
-        $hashtable[$key] = $value
+        $hashtable[$key] = "$value"
     }
 
     # Output the hashtable
     $hashtable.GetEnumerator() | ForEach-Object {
-        $string = '{0}={1}' -f $_.Key, $_.Value
+        $string = '{0}={1}' -f $_.Key, "$($_.Value)"
         Write-Output $string >> $Env:GITHUB_OUTPUT
     }
 }
