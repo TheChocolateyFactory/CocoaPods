8d577ba716fc91036a1a209fd7c74de5fcd364e7
Stephen Valdinger <stephen@chocolatey.io>
2025-06-09 22:16:31 -0400

    Initial commit of package workflow

---
+++ build.ps1
@@ -0,0 +1,83 @@
+[CmdletBinding()]
+Param(
+
+    [Parameter(Mandatory)]
+    [String]
+    $PackageId,
+
+    [Parameter(Mandatory)]
+    [String]
+    $PackageVersion,
+
+    [Parameter(Mandatory)]
+    [String]
+    $Url,
+
+    [Parameter(Mandatory)]
+    [String]
+    $SoftwareName,
+
+    [Parameter(Mandatory)]
+    [String]
+    $SilentArgs,
+
+    [Parameter(Mandatory)]
+    [String]
+    $ValidExitCodes,
+
+    [Parameter(Mandatory)]
+    [String]
+    $Author,
+
+    [Parameter(Mandatory)]
+    [String]
+    $InstallerType,
+
+    [Parameter(Mandatory)]
+    [String]
+    $Summary,
+
+    [Parameter(Mandatory)]
+    [String]
+    $Description
+
+)
+
+end {
+    $tempDir = Join-Path $env:TEMP -ChildPath (New-Guid).Guid
+
+    # Create our temp dir
+    if (-not (Test-Path $tempDir)) {
+        $null = New-Item $tempDir -ItemType Directory
+    }
+
+    # Checksum our file
+    $file = Split-path $Url -Leaf
+
+    $download = Join-Path $tempDir -ChildPath $file
+    [System.Net.WebClient]::new().DownloadFile($Url, $download)
+
+    $checksum = (Get-FileHash $download -Algorithm SHA256).Hash
+    
+    if ($checksum) {
+        Remove-item $download -Force
+    }
+
+    # Get path to choco.exe
+    $choco = (Get-Command choco).source
+
+    if (-not $choco) {
+        throw "Chocolatey CLI is required but not found!"
+    }
+
+    # Make our package from a template
+    choco new $PackageId --version="$PackageVersion" --template='webinar' Url="$Url" SoftwareName="$SoftwareName" silentArgs="$SilentArgs" validExitCodes="$ValidExitCodes" Checksum="$checksum" Author="$Author" InstallerType="$InstallerType" Summary="$Summary" Description="$Description" --output-directory="$tempDir" --build-package
+    $nuspec = (Get-ChildItem $tempDir -Recurse -Filter '*.nuspec').FullName
+
+    # Pack the bitch
+    choco pack $nuspec --output-directory="$tempDir"
+    # Output our temp dir for the publish step
+    Write-Output "TempDir=$($tempDir)" >> $Env:GITHUB_OUTPUT
+
+    return $Env:GITHUB_OUTPUT
+}
---
+++ publish.ps1
@@ -0,0 +1,36 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory)]
+    [String]
+    $TempDir,
+
+    [Parameter(Mandatory)]
+    [String]
+    $RepositoryUrl,
+
+    [Parameter(Mandatory)]
+    [String]
+    $ApiKey
+)
+
+end {
+    if (-not (Test-Path $TempDir)) {
+        throw "$TempDir does not exist! I fear catastrophe has struck!"
+    }
+
+    $choco = (Get-Command choco).Source
+
+    if (-not $choco) {
+        throw 'Ruh-roh, raggy! Chocolatey CLI is required, but was not found!'
+    }
+
+    $nupkg = (Get-ChildItem $TempDir -Filter '*.nupkg' -Recurse).FullName
+
+    $chocoArgs = @('push', $nupkg, "--source='$RepositoryUrl'", "--api-key='$ApiKey'")
+
+    & choco @chocoArgs
+
+    if ($LASTEXITCODE -eq 0) {
+        Remove-Item $TempDir -Recurse -Force
+    }
+}
